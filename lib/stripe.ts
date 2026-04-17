068db0f4fc2947c4c85b2b78ee871344c90d3cf2
Isaac <stargitsolutions@gmail.com>
2026-04-17 21:28:10 +0100

    fix: update Stripe API version to fix build error

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -5,5 +5,5 @@ if (!process.env.STRIPE_SECRET_KEY) {
 }
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-  apiVersion: "2025-03-31.basil",
+  apiVersion: "2026-03-25.dahlia",
 });
